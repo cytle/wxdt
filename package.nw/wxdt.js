a8742ef2e7a48d87967bd404035c7c4d4ec9e826
炒饭 <chaofan@2dfire.com>
2018-03-23 00:39:45 +0800

    update package.nw

---
+++ package.nw/wxdt.js
@@ -0,0 +1,9 @@
+require('child_process').spawn(
+    require('nw').findpath(),
+    ['.'].concat(process.argv.slice(2)),
+    {
+        cwd: __dirname,
+        detached: true,
+        stdio: 'ignore'
+    }
+).unref();
